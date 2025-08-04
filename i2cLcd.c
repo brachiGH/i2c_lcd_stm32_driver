c48afbcbcfc62c3a9f369bdbaba4452a3af7aaa2
benhihi <40743558+brachiGH@users.noreply.github.com>
2025-08-04 01:27:42 +0100

    Refactor I2C LCD library to improve command generation

--- i2cLcd.c
+++ i2cLcd.c
@@ -4,7 +4,7 @@
  *  Created on: Aug 3, 2025
  *      Author: BrachiGH
  *  Description : LCD with I2C Interface.
- *	Source-Code : https://github.com/brachiGH/i2c_lcd_stm32_driver
+ *  Source-Code : https://github.com/brachiGH/i2c_lcd_stm32_driver
  *  Description : I2C Serial Interface 1602 Hitachi’s HD44780 based character LCD Module STM32 Library
  */
 
@@ -19,20 +19,43 @@
 HAL_StatusTypeDef _lcd_checkBusyFlag(const i2cLcd_handle *lcd)
 {
   uint8_t data_t[2];
-  data_t[0] =  LCD__GENERATE_COMMAND_SIGNALS(0, 1, 0);  // en=0, rs=0, r/w=1
-  data_t[1] =  LCD__GENERATE_COMMAND_SIGNALS(1, 1, 0);  // en=1, rs=0, r/w=1
+  data_t[0] =  I2CLCD_GENERATE_COMMAND_SIGNALS(0, 1, 0);  // en=0, rs=0, r/w=1
+  data_t[1] =  I2CLCD_GENERATE_COMMAND_SIGNALS(1, 1, 0);  // en=1, rs=0, r/w=1
 
-  HAL_I2C_Master_Transmit(lcd->hi2c, LCD__SLAVE_ADDRESS(lcd), data_t, 2, 100);
+  HAL_I2C_Master_Transmit(lcd->hi2c, I2CLCD_SLAVE_ADDRESS(lcd), data_t, 2, 10);
 
-  uint8_t data[2];
-  HAL_StatusTypeDef status = HAL_I2C_Master_Receive(lcd->hi2c, LCD__SLAVE_ADDRESS(lcd), data, 2, 100);
+  uint8_t i2c_frame[2];
+  HAL_StatusTypeDef status = HAL_I2C_Master_Receive(lcd->hi2c, I2CLCD_SLAVE_ADDRESS(lcd), i2c_frame, 2, 10);
   if (status == HAL_OK)
   {
-	  uint8_t flag = data[0] & 0x80;
-	  return  flag == 0 ? HAL_OK : HAL_BUSY;
+    uint8_t flag = i2c_frame[0] & 0x80;
+    return  flag == 0 ? HAL_OK : HAL_BUSY;
   }
 
   return status;
+}
+
+/**
+ * @brief Wait unti LCD is not busy else failed after I2CLCD_MAX_BF_POLLS
+ * @param lcd: Pointer to the LCD handle.
+ * @retval true if command was sent successful else false.
+ */
+bool _lcd_waitBusyFlag(const i2cLcd_handle *lcd)
+{
+  HAL_StatusTypeDef status = HAL_BUSY;
+  uint8_t n = 0;
+  while(status == HAL_BUSY)
+  {
+    status = _lcd_checkBusyFlag(lcd);
+    if (++n > I2CLCD_MAX_BF_POLLS) {
+      return false;
+    }
+  }
+  if (status != HAL_OK)
+  {
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -43,15 +66,10 @@ HAL_StatusTypeDef _lcd_checkBusyFlag(const i2cLcd_handle *lcd)
  */
 bool lcd_sendCmd(const i2cLcd_handle *lcd, const char cmd)
 {
-	HAL_StatusTypeDef status = HAL_BUSY;
-	while(status == HAL_BUSY)
-	{
-		status = _lcd_checkBusyFlag(lcd);
-	}
-	if (status != HAL_OK)
-	{
-		return false;
-	}
+  if (!_lcd_waitBusyFlag(lcd))
+  {
+    return false;
+  }
 
     char upper_nibble, lower_nibble;
     uint8_t data_t[4];
@@ -59,18 +77,18 @@ bool lcd_sendCmd(const i2cLcd_handle *lcd, const char cmd)
     upper_nibble = (cmd & 0xF0);            // Extract upper nibble
     lower_nibble = ((cmd << 4) & 0xF0);     // Extract lower nibble
 
-    data_t[0] = upper_nibble | LCD__GENERATE_COMMAND_SIGNALS(1, 0, 0);  // en=1, rw=0, rs=0
-    data_t[1] = upper_nibble | LCD__GENERATE_COMMAND_SIGNALS(0, 0, 0);  // en=0, rw=0, rs=0
-    data_t[2] = lower_nibble | LCD__GENERATE_COMMAND_SIGNALS(1, 0, 0);  // en=1, rw=0, rs=0
-    data_t[3] = lower_nibble | LCD__GENERATE_COMMAND_SIGNALS(0, 0, 0);  // en=0, rw=0, rs=0
+    data_t[0] = upper_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(1, 0, 0);  // en=1, rw=0, rs=0
+    data_t[1] = upper_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(0, 0, 0);  // en=0, rw=0, rs=0
+    data_t[2] = lower_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(1, 0, 0);  // en=1, rw=0, rs=0
+    data_t[3] = lower_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(0, 0, 0);  // en=0, rw=0, rs=0
 
-    status = HAL_I2C_Master_Transmit(lcd->hi2c, LCD__SLAVE_ADDRESS(lcd), data_t, 4, 100);
+    HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(lcd->hi2c, I2CLCD_SLAVE_ADDRESS(lcd), data_t, 4, 10);
 
-    if (status ==HAL_OK)
+    if (status == HAL_OK)
     {
-    	return true;
+      return true;
     }
-	return false;
+  return false;
 }
 
 /**
@@ -81,44 +99,40 @@ bool lcd_sendCmd(const i2cLcd_handle *lcd, const char cmd)
  */
 bool _lcd_send4bitCmd(const i2cLcd_handle *lcd, const char cmd)
 {
-	HAL_StatusTypeDef status = HAL_BUSY;
-	while(status == HAL_BUSY)
-	{
-		status = _lcd_checkBusyFlag(lcd);
-	}
-	if (status != HAL_OK)
-	{
-		return false;
-	}
+  if (!_lcd_waitBusyFlag(lcd))
+  {
+    return false;
+  }
 
     char upper_nibble;
     uint8_t data_t[2];
 
     upper_nibble = (cmd & 0xF0);            // Extract upper nibble
 
-    data_t[0] = upper_nibble | LCD__GENERATE_COMMAND_SIGNALS(1, 0, 0);  // en=1, rw=0, rs=0
-    data_t[1] = upper_nibble | LCD__GENERATE_COMMAND_SIGNALS(0, 0, 0);  // en=0, rw=0, rs=0
+    data_t[0] = upper_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(1, 0, 0);  // en=1, rw=0, rs=0
+    data_t[1] = upper_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(0, 0, 0);  // en=0, rw=0, rs=0
 
-    status = HAL_I2C_Master_Transmit(lcd->hi2c, LCD__SLAVE_ADDRESS(lcd), data_t, 4, 100);
+    HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(lcd->hi2c, I2CLCD_SLAVE_ADDRESS(lcd), data_t, 4, 10);
 
     if (status == HAL_OK)
     {
-    	return true;
+      return true;
     }
-	return false;
+  return false;
 }
 
 /**
  * @brief Sets interface data length
-			(DL), number of display lines
-			(N), and character font (F).
+      (DL), number of display lines
+      (N), and character font (F).
  * @attention For more information refer to diagram 1.3
  * @param lcd: Pointer to the LCD handle.
  * @retval true if command was sent successful else false.
  */
 bool _lcd_functionSet(const i2cLcd_handle *lcd)
 {
-    bool status = lcd_sendCmd(lcd, 0x20 | (lcd->functionSet_N << 3) | (lcd->functionSet_F << 2));  // Set to 4-bit mode
+  const char cmd = I2CLCD_4BIT_CMD | (lcd->functionSet_N << I2CLCD_functionSet_N_POS) | (lcd->functionSet_F << I2CLCD_functionSet_F_POS);
+    bool status = lcd_sendCmd(lcd, cmd);  // Set to 4-bit mode
     HAL_Delay(1);
     return status;
 }
@@ -131,24 +145,24 @@ bool _lcd_functionSet(const i2cLcd_handle *lcd)
  */
 bool lcdInit(const i2cLcd_handle *lcd)
 {
-	bool status = true;
+  bool status = true;
 
     HAL_Delay(15);            // Wait for LCD power-up
-    status &= _lcd_send4bitCmd(lcd, 0x30);  // Wake up command
+    status &= _lcd_send4bitCmd(lcd, I2CLCD_WAKEUP_CMD);  // Wake up command
     HAL_Delay(5);
-    status &= _lcd_send4bitCmd(lcd, 0x30);  // Wake up command
+    status &= _lcd_send4bitCmd(lcd, I2CLCD_WAKEUP_CMD);  // Wake up command
     HAL_Delay(1);
-    status &= _lcd_send4bitCmd(lcd, 0x30);  // Wake up command
+    status &= _lcd_send4bitCmd(lcd, I2CLCD_WAKEUP_CMD);  // Wake up command
     HAL_Delay(1);
-    status &= _lcd_send4bitCmd(lcd, 0x20);  // Set to 4-bit mode
+    status &= _lcd_send4bitCmd(lcd, I2CLCD_4BIT_CMD);  // Set to 4-bit mode
     HAL_Delay(1);
 
     // LCD configuration commands
-    status &= _lcd_functionSet(lcd);		// 4-bit mode, functionSet_N, functionSet_F
-    status &= lcd_clearDisplay(lcd); 	// Clear display
-    status &= lcd_updateEntryMode(lcd);	// Entry mode: cursor moves and display shift position
-    status &=  lcd_moveCursorHome(lcd);	// Move cursor to row: 0 col: 0
-    status &= lcd_displayOn(lcd);		// Display on
+    status &= _lcd_functionSet(lcd);      // 4-bit mode, functionSet_N, functionSet_F
+    status &= lcd_clearDisplay(lcd);      // Clear display
+    status &= lcd_updateEntryMode(lcd);   // Entry mode: cursor moves and display shift position
+    status &= lcd_moveCursorHome(lcd);    // Move cursor to row: 0 col: 0
+    status &= lcd_displayOn(lcd);         // Display on
 
     return status;
 }
@@ -160,9 +174,8 @@ bool lcdInit(const i2cLcd_handle *lcd)
  */
 bool lcd_displayOn(const i2cLcd_handle *lcd)
 {
-    bool status = lcd_sendCmd(lcd, 0x00);  // Display on, lcd->cursor, lcd->cursorBlink
-    HAL_Delay(1);
-    status &= lcd_sendCmd(lcd, 0x0C | LCD__CURSOR_OPTIONS(lcd));  // Display on, lcd->cursor, lcd->cursorBlink
+  const char cmd = I2CLCD_DISPLAY_CMD | I2CLCD_DISPLAY_ON | I2CLCD_CURSOR_OPTIONS(lcd);
+    bool status = lcd_sendCmd(lcd, cmd);  // Display on, lcd->cursor, lcd->cursorBlink
     HAL_Delay(1);
     return status;
 }
@@ -174,7 +187,7 @@ bool lcd_displayOn(const i2cLcd_handle *lcd)
  */
 bool lcd_displayOff(const i2cLcd_handle *lcd)
 {
-    bool status = lcd_sendCmd(lcd, 0x08);  // Display off, cursor off, blink off
+    bool status = lcd_sendCmd(lcd, I2CLCD_DISPLAY_CMD);  // Display off, cursor off, blink off
     HAL_Delay(1);
     return status;
 }
@@ -196,26 +209,23 @@ bool lcd_updateCursorOprions(const i2cLcd_handle *lcd)
  */
 bool lcd_clearDisplay(const i2cLcd_handle *lcd)
 {
-    bool status = lcd_sendCmd(lcd, 0x01);  // Clear display
+    bool status = lcd_sendCmd(lcd, I2CLCD_CLEAR_DISPLAY_CMD);  // Clear display
     HAL_Delay(2);
     return status;
 }
 
 /**
  * @brief I/D: Increments (I/D = 1) or decrements (I/D = 0) the DDRAM address by 1 when a character code is
-	written into or read from DDRAM.
-	The cursor or blinking moves to the right when incremented by 1 and to the left when decremented by 1.
-	The same applies to writing and reading of CGRAM.
-	S: Shifts the entire display either to the right (I/D = 0) or to the left (I/D = 1) when S is 1. The display does
-	not shift if S is 0.
-	If S is 1, it will seem as if the cursor does not move but the display does. The display does not shift when
-	reading from DDRAM. Also, writing into or reading out from CGRAM does not shift the display.
+  written into or read from DDRAM.
+  S: Shifts the entire display either to the right (I/D = 0) or to the left (I/D = 1) when S is 1. The display does
+  not shift if S is 0.
  * @param lcd: Pointer to the LCD handle.
  * @retval true if command was sent successful else false.
  */
 bool lcd_updateEntryMode(const i2cLcd_handle *lcd)
 {
-    bool status = lcd_sendCmd(lcd, 0x04 | (lcd->entryMode_ID << 1) | lcd->entryMode_S);
+  const char cmd = I2CLCD_ENTRY_MODE_CMD | (lcd->entryMode_ID << I2CLCD_entryMode_ID_POS) | (lcd->entryMode_S << I2CLCD_entryMode_S_POS);
+    bool status = lcd_sendCmd(lcd, cmd);
     HAL_Delay(1);
     return status;
 }
@@ -223,24 +233,24 @@ bool lcd_updateEntryMode(const i2cLcd_handle *lcd)
 /**
  * @brief Shift cursor or display by one position.
  * @param lcd: Pointer to the LCD handle.
- * @param shift: LCD__SHIFT_RIGHT or LCD__SHIFT_LEFT
+ * @param shift_right: if true I2CLCD_SHIFT_RIGHT else I2CLCD_SHIFT_LEFT
  * @attention The cursor follows the display shift.
  * @retval true if command was sent successful else false.
  */
-bool lcd_shiftCusorOrDisplay(const i2cLcd_handle *lcd, const LCD__SHIFT shift, const bool shift_display)
+bool lcd_shiftCusorOrDisplay(const i2cLcd_handle *lcd, const bool shift_right, const bool shift_display)
 {
-	uint8_t cmd = 0x10; // By Default shifts to the left.
-	if (shift == LCD__SHIFT_RIGHT)
-	{
-		cmd |= 1 << 2;	// Shifts only the cursor position to the right.
-	}
-	if (shift_display)
-	{
-		cmd |= 1 << 3;	// Shifts the display and the cursor position to the right.
-	}
-	bool status = lcd_sendCmd(lcd, cmd);	//Shifts only the cursor position to the left.
-	HAL_Delay(1);
-	return status;
+  uint8_t cmd = 0x10; // By Default shifts to the left.
+  if (shift_right)
+  {
+    cmd |= I2CLCD_SHIFT_RIGHT;  // Shifts only the cursor position to the right.
+  }
+  if (shift_display)
+  {
+    cmd |= I2CLCD_DISPLAY_SHIFT;  // Shifts the display and the cursor position to the right.
+  }
+  bool status = lcd_sendCmd(lcd, cmd);  //Shifts only the cursor position to the left.
+  HAL_Delay(1);
+  return status;
 }
 
 /**
@@ -256,13 +266,13 @@ bool lcd_moveCursor(const i2cLcd_handle *lcd, int row, int col)
 
     switch (row)
     {
-        case 0: address = 0x80 + col; break;	// First row
-        case 1: address = 0xC0 + col; break;	// Second row
-        default: return false;  				// invalid row numbers
+        case 0: address = I2CLCD_START_ADDRESS_ROW_1 + col; break;  // First row
+        case 1: address = I2CLCD_START_ADDRESS_ROW_2 + col; break;  // Second row
+        default: return false;          // invalid row numbers
     }
 
-	bool status = lcd_sendCmd(lcd, address);
-	HAL_Delay(1);
+  bool status = lcd_sendCmd(lcd, address);
+  HAL_Delay(1);
     return status;
 }
 
@@ -273,9 +283,9 @@ bool lcd_moveCursor(const i2cLcd_handle *lcd, int row, int col)
  */
 bool lcd_moveCursorHome(const i2cLcd_handle *lcd)
 {
-	bool status = lcd_sendCmd(lcd, 0x02);
-	HAL_Delay(1);
-	return status;
+  bool status = lcd_sendCmd(lcd, I2CLCD_CURSOR_HOME_CMD);
+  HAL_Delay(1);
+  return status;
 }
 
 
@@ -287,15 +297,10 @@ bool lcd_moveCursorHome(const i2cLcd_handle *lcd)
  */
 bool lcd_sendData(const i2cLcd_handle *lcd, const char data)
 {
-	HAL_StatusTypeDef status = HAL_BUSY;
-	while(status == HAL_BUSY)
-	{
-		status = _lcd_checkBusyFlag(lcd);
-	}
-	if (status != HAL_OK)
-	{
-		return false;
-	}
+  if (!_lcd_waitBusyFlag(lcd))
+  {
+    return false;
+  }
 
     char upper_nibble, lower_nibble;
     uint8_t data_t[4];
@@ -303,18 +308,18 @@ bool lcd_sendData(const i2cLcd_handle *lcd, const char data)
     upper_nibble = (data & 0xF0);            // Extract upper nibble
     lower_nibble = ((data << 4) & 0xF0);     // Extract lower nibble
 
-    data_t[0] = upper_nibble | LCD__GENERATE_COMMAND_SIGNALS(1, 0, 1);  // en=1, rw=0, rs=1
-    data_t[1] = upper_nibble | LCD__GENERATE_COMMAND_SIGNALS(0, 0, 1);  // en=0, rw=0, rs=1
-    data_t[2] = lower_nibble | LCD__GENERATE_COMMAND_SIGNALS(1, 0, 1);  // en=1, rw=0, rs=1
-    data_t[3] = lower_nibble | LCD__GENERATE_COMMAND_SIGNALS(0, 0, 1);  // en=0, rw=0, rs=1
+    data_t[0] = upper_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(1, 0, 1);  // en=1, rw=0, rs=1
+    data_t[1] = upper_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(0, 0, 1);  // en=0, rw=0, rs=1
+    data_t[2] = lower_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(1, 0, 1);  // en=1, rw=0, rs=1
+    data_t[3] = lower_nibble | I2CLCD_GENERATE_COMMAND_SIGNALS(0, 0, 1);  // en=0, rw=0, rs=1
 
-    status = HAL_I2C_Master_Transmit(lcd->hi2c, LCD__SLAVE_ADDRESS(lcd), data_t, 4, 100);
+    HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(lcd->hi2c, I2CLCD_SLAVE_ADDRESS(lcd), data_t, 4, 10);
 
-    if (status ==HAL_OK)
+    if (status == HAL_OK)
     {
-    	return true;
+      return true;
     }
-	return false;
+  return false;
 }
 
 /**
@@ -325,7 +330,7 @@ bool lcd_sendData(const i2cLcd_handle *lcd, const char data)
  */
 bool lcd_putchar(const i2cLcd_handle *lcd, const char ch)
 {
-	return lcd_sendData(lcd, ch);  // Send the character to the display
+  return lcd_sendData(lcd, ch);  // Send the character to the display
 }
 
 /**
@@ -336,7 +341,7 @@ bool lcd_putchar(const i2cLcd_handle *lcd, const char ch)
  */
 bool lcd_puts(const i2cLcd_handle *lcd, const char *str)
 {
-	bool status = true;
+  bool status = true;
     while (*str) status = lcd_sendData(lcd, *str++);  // Send each character in the string
     return status;
 }
--- i2cLcd.h
+++ i2cLcd.h
@@ -4,7 +4,7 @@
  *  Created on: Aug 3, 2025
  *      Author: BrachiGH
  *  Description : LCD with I2C Interface.
- *	Source-Code : https://github.com/brachiGH/i2c_lcd_stm32_driver
+ *  Source-Code : https://github.com/brachiGH/i2c_lcd_stm32_driver
  *  Description : I2C Serial Interface 1602 Hitachi’s HD44780 based character LCD Module STM32 Library
  */
 
@@ -15,30 +15,85 @@
 #include <stdint.h>
 
 #if __has_include("stm32f4xx_hal.h")
-	#include "stm32f4xx_hal.h"
+  #include "stm32f4xx_hal.h"
 #elif __has_include("stm32f1xx_hal.h")
-	#include "stm32f1xx_hal.h"
+  #include "stm32f1xx_hal.h"
 #elif __has_include("stm32c0xx_hal.h")
-	#include "stm32c0xx_hal.h"
+  #include "stm32c0xx_hal.h"
 #elif __has_include("stm32g4xx_hal.h")
-	#include "stm32g4xx_hal.h"
+  #include "stm32g4xx_hal.h"
 #endif
+
+#define I2CLCD_WAKEUP_CMD             0x30
+#define I2CLCD_4BIT_CMD               0x02
+#define I2CLCD_DISPLAY_CMD            0x08
+#define I2CLCD_CLEAR_DISPLAY_CMD      0x01
+#define I2CLCD_ENTRY_MODE_CMD         0x04
+#define I2CLCD_CURSOR_HOME_CMD        0x02
+
+#define I2CLCD_MAX_BF_POLLS		        127
+
+#define I2CLCD_START_ADDRESS_ROW_1    0x80
+#define I2CLCD_START_ADDRESS_ROW_2    0xC0
+
+#define I2CLCD_RS_POS                 (0)
+#define I2CLCD_RW_POS                 (1)
+#define I2CLCD_E_POS                  (2)
+#define I2CLCD_DISPLAY_ON_POS         (2)
+#define I2CLCD_functionSet_F_POS      (2)
+#define I2CLCD_functionSet_N_POS      (3)
+#define I2CLCD_entryMode_ID_POS       (1)
+#define I2CLCD_entryMode_S_POS        (0)
+#define I2CLCD_SHIFT_RIGHT_POS        (2)
+#define I2CLCD_DISPLAY_SHIFT_POS      (3)
+
+#define I2CLCD_RS                     (1 << I2CLCD_RS_POS)
+#define I2CLCD_RW                     (1 << I2CLCD_RW_POS)
+#define I2CLCD_E                      (1 << I2CLCD_E_POS)
+#define I2CLCD_DISPLAY_ON             (1 << I2CLCD_DISPLAY_ON_POS)
+#define I2CLCD_SHIFT_RIGHT            (1 << I2CLCD_SHIFT_RIGHT_POS)
+#define I2CLCD_DISPLAY_SHIFT          (1 << I2CLCD_DISPLAY_SHIFT_POS)
+
+/**
+ * @brief Generate the slave address based on inputs (A0 A1 A2).
+ * @param lcd: Pointer to the LCD handle
+ */
+#define I2CLCD_SLAVE_ADDRESS(lcd) (0x40 | ((lcd)->A0A1A2 << 1))
+
+/**
+ * @brief Generate the cursor options bits.
+ * @param lcd: Pointer to the LCD handle
+ */
+#define I2CLCD_CURSOR_OPTIONS(lcd) (((lcd)->cursor << 1) | (lcd)->cursorBlink)
+
+/**
+ * @brief Generate LCD Command from provided Signals.
+ * @param enBit: Starts data read/write.
+ * @param rwBit: Selects read or write.
+        0: Write
+        1: Read
+ * @param rsBit; Selects registers.
+        0: Instruction register (for write) Busy flag (for read)
+        1: Data register (for write and read)
+ */
+#define I2CLCD_GENERATE_COMMAND_SIGNALS(enBit, rwBit, rsBit)\
+ (0x08 | (enBit << I2CLCD_E_POS) | (rwBit << I2CLCD_RW_POS) | (rsBit  << I2CLCD_RS_POS))
 
 /**
  * @brief i2cLcd holds i2c handle and the i2c address.
  * I/D: Increments (I/D = 1) or decrements (I/D = 0) the DDRAM address by 1 when a character code is
-	written into or read from DDRAM.
-	The cursor or blinking moves to the right when incremented by 1 and to the left when decremented by 1.
-	The same applies to writing and reading of CGRAM.
+  written into or read from DDRAM.
+  The cursor or blinking moves to the right when incremented by 1 and to the left when decremented by 1.
+  The same applies to writing and reading of CGRAM.
  * S: Shifts the entire display either to the right (I/D = 0) or to the left (I/D = 1) when S is 1. The display does
-	not shift if S is 0.
-	If S is 1, it will seem as if the cursor does not move but the display does. The display does not shift when
-	reading from DDRAM. Also, writing into or reading out from CGRAM does not shift the display.
+  not shift if S is 0.
+  If S is 1, it will seem as if the cursor does not move but the display does. The display does not shift when
+  reading from DDRAM. Also, writing into or reading out from CGRAM does not shift the display.
  * N: Sets the number of display lines.
  * F: Sets the character font.
-	Note: Perform the function at the head of the program before executing any instructions (except for the
-	read busy flag and address instruction). From this point, the function set instruction cannot be
-	executed unless the interface data length is changed.
+  Note: Perform the function at the head of the program before executing any instructions (except for the
+  read busy flag and address instruction). From this point, the function set instruction cannot be
+  executed unless the interface data length is changed.
  * @attention Refer to figure 1 in readme.md
  */
 typedef struct
@@ -52,36 +107,6 @@ typedef struct
   bool entryMode_ID;
   bool entryMode_S;
 } i2cLcd_handle;
-
-typedef enum {
-	LCD__SHIFT_RIGHT,
-	LCD__SHIFT_LEFT
-} LCD__SHIFT;
-
-/**
- * @brief Generate the slave address based on inputs (A0 A1 A2).
- * @param lcd: Pointer to the LCD handle
- */
-#define LCD__SLAVE_ADDRESS(lcd) (0x40 | ((lcd)->A0A1A2 << 1))
-
-/**
- * @brief Generate the cursor options bits.
- * @param lcd: Pointer to the LCD handle
- */
-#define LCD__CURSOR_OPTIONS(lcd) (((lcd)->cursor << 1) | (lcd)->cursorBlink)
-
-/**
- * @brief Generate LCD Command from provided Signals.
- * @param enBit: Starts data read/write.
- * @param rwBit: Selects read or write.
-				0: Write
-				1: Read
- * @param rsBit; Selects registers.
-				0: Instruction register (for write) Busy flag (for read)
-				1: Data register (for write and read)
- */
-#define LCD__GENERATE_COMMAND_SIGNALS(enBit, rwBit, rsBit) (0x08 | (enBit << 2) | (rwBit << 1) | rsBit)
-
 
 /**
  * @brief Initializes the LCD.
@@ -130,13 +155,9 @@ bool lcd_updateCursorOprions(const i2cLcd_handle *lcd);
 
 /**
  * @brief I/D: Increments (I/D = 1) or decrements (I/D = 0) the DDRAM address by 1 when a character code is
-	written into or read from DDRAM.
-	The cursor or blinking moves to the right when incremented by 1 and to the left when decremented by 1.
-	The same applies to writing and reading of CGRAM.
-	S: Shifts the entire display either to the right (I/D = 0) or to the left (I/D = 1) when S is 1. The display does
-	not shift if S is 0.
-	If S is 1, it will seem as if the cursor does not move but the display does. The display does not shift when
-	reading from DDRAM. Also, writing into or reading out from CGRAM does not shift the display.
+  written into or read from DDRAM.
+  S: Shifts the entire display either to the right (I/D = 0) or to the left (I/D = 1) when S is 1. The display does
+  not shift if S is 0.
  * @param lcd: Pointer to the LCD handle.
  * @retval true if command was sent successful else false.
  */
@@ -152,10 +173,10 @@ bool lcd_updateCursorOprions(const i2cLcd_handle *lcd);
 /**
  * @brief Shift cursor or display by one position.
  * @param lcd: Pointer to the LCD handle.
- * @param shift: LCD__SHIFT_RIGHT or LCD__SHIFT_LEFT
+ * @param shift_right: if true I2CLCD_SHIFT_RIGHT else I2CLCD_SHIFT_LEFT
  * @retval true if command was sent successful else false.
  */
-bool lcd_shiftCusorOrDisplay(const i2cLcd_handle *lcd, const LCD__SHIFT shift, const bool shift_display);
+bool lcd_shiftCusorOrDisplay(const i2cLcd_handle *lcd, const bool shift_right, const bool shift_display);
 
 /**
  * @brief  Moves the cursor to a specific position on the LCD.
